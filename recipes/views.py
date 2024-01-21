49dfa965f22b23145f9c58e7f2e7ea37919ea423
Leonardo Rocha <ep.leonardorocha@gmail.com>
2024-01-21 09:57:35 -0300

    Partials-create

--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 def home(request):
-    return render(request, 'recipes/home.html')
+    return render(request, 'recipes/pages/home.html', context={
+        'name': 'Leonardo Rocha'
+    })
