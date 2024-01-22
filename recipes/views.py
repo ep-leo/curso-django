a57235ea325615038a573a7570a8bacb18ae69b9
Leonardo Rocha <ep.leonardorocha@gmail.com>
2024-01-22 16:34:05 -0300

    recipe-view

--- recipes/views.py
+++ recipes/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'recipes/pages/home.html', context={
-        'name': 'Leonardo Rocha'
-    })
+    return render(request, 'recipes/pages/home.html')
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipe-view.html')
