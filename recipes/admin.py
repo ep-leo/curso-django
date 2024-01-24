1f2967f46c1c2034d6bd2d4ad09167aa35d57f4e
Leonardo Rocha <ep.leonardorocha@gmail.com>
2024-01-24 15:20:23 -0300

    module_models_start

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from .models import Category, Recipe
 
+@admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
   pass
 
+@admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
   pass
 
-admin.site.register(Category, CategoryAdmin)
-admin.site.register(Recipe, RecipeAdmin)
