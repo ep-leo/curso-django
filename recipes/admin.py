7cbe1a294ef1cbffb06a93998e06f746306cbf88
Leonardo Rocha <ep.leonardorocha@gmail.com>
2024-01-25 09:52:21 -0300

    filter-views

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from .models import Category, Recipe
 
-@admin.register(Category)
+
 class CategoryAdmin(admin.ModelAdmin):
   pass
+
+admin.site.register(Category, CategoryAdmin)
 
 @admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
