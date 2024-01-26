82f909dfaf37c1b12ccf10bc95e23c2c0381cda2
Leonardo Rocha <ep.leonardorocha@gmail.com>
2024-01-26 10:10:06 -0300

    ....

--- recipes/views.py
+++ recipes/views.py
@@ -17,6 +17,7 @@ def category(request,category_id):
         is_published=True).order_by('-id')
     return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
+        'title': f'{recipes.first().category.name} - Category | ',
     })
 
 def recipe(request, id):
